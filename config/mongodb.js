0d916c2c13e0d9f2d9761a43803af8fc440ea3b4
SundayNT <azterisk13@gmail.com>
2024-11-29 15:33:46 +0700

    2nd commit

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     mongoose.connection.once("connected", () => {
-        console.log("Connected to MongoDB 🎄🎁");
+        console.log("Connected to MongoDB 🎄🎁 Let's Start ✨");
     });
     await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`);
 };
